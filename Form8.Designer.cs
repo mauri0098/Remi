57ec270f16355c9a85d68f610557765ebb3b0674
root <mgiraudo@ies21.edu.ar>
2023-09-29 13:01:45 -0300

    2225

--- Form8.Designer.cs
+++ Form8.Designer.cs
@@ -70,7 +70,7 @@ namespace Remi
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(332, 110);
+            this.button1.Location = new System.Drawing.Point(332, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 59);
             this.button1.TabIndex = 5;
