0bf941fe0038b0b1bb2c407ce4deabb65423950f
mauri0098 <maurirodriguez0098@gmail.com>
2023-10-06 07:32:24 -0300

    2004

---
+++ parcial.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Remi
+{
+    partial class parcial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CboChofer = new System.Windows.Forms.ComboBox();
+            this.cboMes = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Origen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Destino = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kilometros = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Importe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(523, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(463, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Importe: $";
+            // 
+            // CboChofer
+            // 
+            this.CboChofer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CboChofer.FormattingEnabled = true;
+            this.CboChofer.Location = new System.Drawing.Point(17, 171);
+            this.CboChofer.Name = "CboChofer";
+            this.CboChofer.Size = new System.Drawing.Size(121, 21);
+            this.CboChofer.TabIndex = 13;
+            // 
+            // cboMes
+            // 
+            this.cboMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMes.FormattingEnabled = true;
+            this.cboMes.Items.AddRange(new object[] {
+            "ENERO",
+            "FEBRERO",
+            "MARZO",
+            "ABRIL",
+            "JUNIO",
+            "JULIO",
+            "AGOSTO",
+            "SEPTIEMPRE",
+            "OCTUBRE",
+            "NOVIEMBRE",
+            "DICIEMBRE"});
+            this.cboMes.Location = new System.Drawing.Point(17, 118);
+            this.cboMes.Name = "cboMes";
+            this.cboMes.Size = new System.Drawing.Size(121, 21);
+            this.cboMes.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Fecha,
+            this.Origen,
+            this.Destino,
+            this.Kilometros,
+            this.Importe});
+            this.dataGridView1.Location = new System.Drawing.Point(185, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(581, 283);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Origen
+            // 
+            this.Origen.HeaderText = "Origen";
+            this.Origen.Name = "Origen";
+            this.Origen.Width = 150;
+            // 
+            // Destino
+            // 
+            this.Destino.HeaderText = "Destino";
+            this.Destino.Name = "Destino";
+            this.Destino.Width = 150;
+            // 
+            // Kilometros
+            // 
+            this.Kilometros.HeaderText = "Kilometros";
+            this.Kilometros.Name = "Kilometros";
+            this.Kilometros.Width = 70;
+            // 
+            // Importe
+            // 
+            this.Importe.HeaderText = "Importe";
+            this.Importe.Name = "Importe";
+            this.Importe.Width = 70;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Ver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // parcial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CboChofer);
+            this.Controls.Add(this.cboMes);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "parcial";
+            this.Text = "parcial";
+            this.Load += new System.EventHandler(this.parcial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CboChofer;
+        private System.Windows.Forms.ComboBox cboMes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Origen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Destino;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kilometros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Importe;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ parcial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remi
+{
+    public partial class parcial : Form
+    {
+        public parcial()
+        {
+            InitializeComponent();
+        }
+
+        private void parcial_Load(object sender, EventArgs e)
+        {
+            //carga choferes al combobox
+            REMIS oChoferes = new REMIS();
+            DataTable TabChoferes = oChoferes.getData();
+
+            CboChofer.DisplayMember = "nombre";
+            CboChofer.ValueMember = "chofer";
+            CboChofer.DataSource = TabChoferes;
+
+            //fecha
+            DateTime fechahoy = DateTime.Now;// hace una variable para guardar la fecha de hoy
+            cboMes.SelectedIndex = fechahoy.Month - 2;// pone la fecha en el combobox y le resta para que aparezca el mes actual porque el selected index empieza de 0
+            textBox1.Text = fechahoy.Year.ToString();//pone el año actual en el textbox
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();//limpia la grilla
+            //crea los objetos y las tablas
+            barrio oBarrios = new barrio();
+            DataTable TabBarrios = oBarrios.getData();
+            TotalViajes oViajes = new TotalViajes();
+            DataTable TabViajes = oViajes.getData();
+
+            //crea la variable para acumular
+            Decimal Importetotal = 0;
+            foreach (DataRow filaViaje in TabViajes.Rows)//recorre la tabla viajes
+            {
+                DateTime fecha = Convert.ToDateTime(filaViaje["fecha"]);//hace una variable para guardar las fechas
+                string fechaViaje = fecha.ToString("dd/MM/yyyy");// pasa las fechas a string para ponerlas en formato dd/MM/yyyy
+
+                //if que interpola y pregunta si la variable fechaviaje que guarda las fechas tiene el combobox elegido y el textbox lleno. y pregunta si el nombre
+                //de la tabla de chofer y la elegida en el cbo son iguales
+                if (fechaViaje.Contains($"{(cboMes.SelectedIndex + 2)}/{textBox1.Text}") && filaViaje["chofer"].ToString() == CboChofer.SelectedValue.ToString())
+                {
+                    DataRow filaOrigen = TabBarrios.Rows.Find(filaViaje["desdebarrio"]);//busca en la tabla el origen de donde sale
+                    DataRow filaDestino = TabBarrios.Rows.Find(filaViaje["hastabarrio"]);//busca en la tabla el destino 
+
+                    decimal km = Convert.ToDecimal(filaViaje["km"]);//variable que guarda los kilometros
+                    decimal importe = Convert.ToDecimal(filaViaje["importe"]);//variable que guarda el importe
+
+                    Importetotal += importe;//acumulador de importe
+
+                    dataGridView1.Rows.Add(fechaViaje, filaOrigen["nombre"], filaDestino["nombre"], km.ToString("N2"), importe.ToString("N2"));//muestra en la grilla
+                }
+
+            }
+            if (Importetotal == 0)
+            {
+                label5.Text = "_________";//limpia la etiqueta
+
+            }
+            else
+            {
+                label5.Text = Importetotal.ToString("N2");//muestra el importe total en la etiqueta
+            }
+        }
+    }
+    
+}       
